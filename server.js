6a508cda2ce1cd9de97f277cd4489b121ddfc740
Daniel Chan <iam@danielchan.me>
2017-04-05 22:01:28 -0400

    Views and partial views.

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.listen(3000, function() {
+    console.log("Server listening on port 3000.");
+});
+
+
+app.get("/", function(req, res) {
+    res.send()
+});
