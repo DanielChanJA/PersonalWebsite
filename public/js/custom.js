b0c710240086476da2757f638c5c8b498a7ae5cb
Daniel Chan <iam@danielchan.me>
2017-04-07 05:27:15 -0400

    Work in progress, landing page completed.

---
+++ public/js/custom.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Daniel Chan on 7/4/2017.
+ */
+$(document).ready(function() {
+    $(".rotate").textrotator({
+        animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+        separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+        speed: 3000 // How many milliseconds until the next word show.
+    });
+});
