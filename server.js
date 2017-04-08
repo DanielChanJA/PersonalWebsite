7045cabd4d12fdb54d3912f9fe2ea8d1a46fbce5
Daniel Chan <iam@danielchan.me>
2017-04-08 05:35:03 -0400

    favico

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require("express");
+var favicon = require('serve-favicon');
+var path = require('path');
 var app = express();
 
+
+
 app.use(express.static("public"));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.set("view engine", "ejs");
 
 app.listen(3000, function() {
